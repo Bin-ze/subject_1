2019e51763405512f0a51d27979967630542e7bb
Bin-ze <binze.zero@gmail.com>
2022-10-23 12:02:22 +0800

    subject_1 algorithm

---
+++ env.sh
@@ -0,0 +1,7 @@
+conda create --name openmmlab python=3.8 -y
+conda activate openmmlab
+conda install pytorch torchvision -c pytorch
+pip install -U openmim
+mim install mmcv-full
+pip install -v -e .
+pip install pyrealsense2
