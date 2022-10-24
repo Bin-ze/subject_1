a1f78c20d5977345e93b6ceae7648fc06ef7a344
Bin-ze <binze.zero@gmail.com>
2022-10-24 19:58:21 +0800

    update env

--- env.sh
+++ env.sh
@@ -4,4 +4,5 @@ conda install pytorch torchvision -c pytorch
 pip install -U openmim
 mim install mmcv-full
 pip install -v -e .
-pip install pyrealsense2
+#pip install pyrealsense2
+pip install drpc-0.1.6-py3-none-any.whl
